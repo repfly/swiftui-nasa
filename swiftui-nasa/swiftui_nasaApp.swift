8ae5d720854d78126d466229aaa65559a1fa1323
alper <yldrma9@gmail.com>
2021-10-26 21:11:48 +0300

    Initial Commit

---
+++ swiftui-nasa/swiftui_nasaApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_nasaApp.swift
+//  swiftui-nasa
+//
+//  Created by Alper Yıldırım on 26.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_nasaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swiftui-nasaUITests/swiftui_nasaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  swiftui_nasaUITestsLaunchTests.swift
+//  swiftui-nasaUITests
+//
+//  Created by Alper Yıldırım on 26.10.2021.
+//
+
+import XCTest
+
+class swiftui_nasaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
